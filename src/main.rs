6cf4b106619c5862293e4b517d3fb358183e5f3b
softprops <d.tangren@gmail.com>
2015-11-17 01:40:56 -0500

    better example

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@ use std::sync::Arc;
 use std::thread;
 fn main() {
     let wg = Arc::new(waitgroup::WaitGroup::new());
-    wg.add(1);
-    let wg2 = wg.clone();
-    thread::spawn(move|| {
-        thread::sleep_ms(3000);
-        wg2.done();
-    });
+    for i in 0..100 {
+        wg.add(1);
+        let wg2 = wg.clone();
+        thread::spawn(move|| {
+            thread::sleep_ms(2000);
+            wg2.done();
+        });
+    }
     wg.wait();
     println!("done")
 }
